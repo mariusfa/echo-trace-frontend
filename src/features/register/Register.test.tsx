9eff291a4fea9e53b65b34ac95e4b0fae990fad2
Marius Fagerland <mariusfagerland@yahoo.no>
2023-07-17 23:48:15 +0200

    Create test for register user

--- src/features/register/Register.test.tsx
+++ src/features/register/Register.test.tsx
@@ -1,7 +1,8 @@
 
 import { describe, expect, test } from 'vitest';
-import { render, screen } from '@testing-library/preact';
+import { render, screen, waitFor } from '@testing-library/preact';
 import { Register } from './Register';
+import userEvent from '@testing-library/user-event';
 
 describe('Register test', () => {
     test('should render Register', () => {
@@ -11,5 +12,27 @@ describe('Register test', () => {
         expect(screen.getByText('Password')).toBeDefined()
         expect(screen.getByText('Confirm Password')).toBeDefined()
         expect(screen.getByRole('button', { name: 'Register' })).toBeDefined();
+    })
+
+    test('should do a register user', async () => {
+        render(<Register />)
+
+        const username = screen.getByLabelText('Username')
+        const password = screen.getByLabelText('Password')
+        const confirmPassword = screen.getByLabelText('Confirm Password')
+        const registerButton = screen.getByRole('button', { name: 'Register' })
+        const user = {
+            username: 'test',
+            password: 'test-password',
+        }
+
+        await userEvent.type(username, user.username)
+        await userEvent.type(password, user.password)
+        await userEvent.type(confirmPassword, user.password)
+        await userEvent.click(registerButton)
+
+        await waitFor(() => {
+            expect(screen.getByText('Registration Successful!')).toBeDefined()
+        }, { timeout: 3000 })
     })
 })
