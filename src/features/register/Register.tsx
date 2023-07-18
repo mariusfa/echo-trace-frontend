36b29cc4adb8aecd8e28eafe50b116242e15bd8c
Marius Fagerland <mariusfagerland@yahoo.no>
2023-07-18 22:26:23 +0200

    Implement handle change for register form

--- src/features/register/Register.tsx
+++ src/features/register/Register.tsx
@@ -1,8 +1,23 @@
 import { FunctionalComponent } from 'preact'
 import { Link } from 'preact-router';
+import { useState } from 'preact/hooks';
 
 export const Register: FunctionalComponent = () => {
-    const handleRegister = (event: any) => {
+    const [formValues, setFormValues] = useState({
+        username: '',
+        password: '',
+        confirmPassword: '',
+        });
+
+    const handleChange = (event: Event) => {
+        const {name, value} = event.target as HTMLInputElement;
+        setFormValues({
+            ...formValues,
+            [name]: value
+        })
+    }
+
+    const handleRegister = (event: Event) => {
         event.preventDefault();
         // Here is where you'd handle the registration
     }
@@ -15,19 +30,19 @@ export const Register: FunctionalComponent = () => {
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                             Username
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" name="username" type="text" placeholder="Username" value={formValues.username} onChange={handleChange} />
                     </div>
                     <div class="mb-4">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                             Password
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" />
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="Password" value={formValues.password} onChange={handleChange} />
                     </div>
                     <div class="mb-6">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="confirmPassword">
                             Confirm Password
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirmPassword" type="password" placeholder="Confirm Password" />
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="confirmPassword" name="confirmPassword" type="password" placeholder="Confirm Password" value={formValues.confirmPassword} onChange={handleChange} />
                     </div>
                     <div class="flex items-center justify-between">
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2" type="submit">
