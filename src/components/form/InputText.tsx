75e50ec6b42f2681672694a80384b5d8bb8e0470
Marius Fagerland <mariusfagerland@yahoo.no>
2023-08-06 18:59:33 +0200

    Create InputText component

---
+++ src/components/form/InputText.tsx
@@ -0,0 +1,34 @@
+import { FunctionalComponent } from 'preact';
+
+interface Props {
+    label: string;
+    name: string;
+    value: string;
+    onChange?: (event: Event) => void;
+    type?: string;
+    readonly?: boolean;
+    
+}
+
+export const InputText: FunctionalComponent<Props> = ({
+    label,
+    name,
+    value,
+    onChange,
+    type,
+    readonly
+}) => {
+    return (
+            <input 
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+                id={name} 
+                name={name} 
+                type={type} 
+                placeholder={label} 
+                value={value} 
+                onChange={onChange} 
+                readOnly={readonly}
+            />
+    
+    )
+}
--- src/components/form/InputTextGroup.tsx
+++ src/components/form/InputTextGroup.tsx
@@ -1,4 +1,5 @@
 import { FunctionalComponent } from 'preact';
+import { InputText } from './InputText';
 
 interface Props {
     label: string;
@@ -15,7 +16,7 @@ export const InputTextGroup: FunctionalComponent<Props> = ({ label, name, value,
             <label class="block text-gray-700 text-sm font-bold mb-2" for={name}>
                 {label}
             </label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id={name} name={name} type={type} placeholder={label} value={value} onChange={onChange} />
+            <InputText label={label} name={name} value={value} onChange={onChange} type={type} />
             {error && <p class="text-red-500 text-xs italic">{error}</p>}
         </div>
     );
