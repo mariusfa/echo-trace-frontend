fc9d379b9578a28266b0c4558b3576f54d8d9299
Marius Fagerland <mariusfagerland@yahoo.no>
2023-08-07 18:45:57 +0200

    Implement fetch token

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,8 +1,21 @@
 import { FunctionalComponent } from 'preact'
 import { RoundedBoxContainer } from '../../components/containers/RoundedBoxContainer'
 import { Heading1 } from '../../components/typography/Heading1'
+import { useEffect, useState } from 'preact/hooks';
+import { fetchWrapper } from '../../wrappers/fetchWrapper';
 
 export const Profile: FunctionalComponent = () => {
+    const [apiToken, setApiToken] = useState('');
+
+    useEffect(() => {
+        const getApiToken = async () => {
+            const response = await fetchWrapper.getJson('/user/api-token');
+            setApiToken(response.data.token);
+        }
+
+        getApiToken();
+    }, []);
+
     return (
         <RoundedBoxContainer md={true}>
             <Heading1 title='Profile' />
@@ -14,11 +27,11 @@ export const Profile: FunctionalComponent = () => {
                     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     id="apiToken"
                     type="text"
-                    value={"api-token"}
+                    value={apiToken}
                     readOnly
                 />
                 <p class="mt-2 text-gray-600 text-sm">
-                    Use this token to post events to <code>apiUrl/events</code> with DTO <code>{'{'}name: eventName{'}'}</code> and header <code>Authorization "API {"api-token"}"</code>.
+                    Use this token to post events to <code>apiUrl/events</code> with DTO <code>{'{'}name: eventName{'}'}</code> and header <code>Authorization "API {apiToken}"</code>.
                 </p>
             </div>
 
