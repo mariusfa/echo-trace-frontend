c4b8d2e71e28e69221d5a80a608f8c0b0d6a7036
Marius Fagerland <mariusfagerland@yahoo.no>
2023-08-08 18:56:51 +0200

    Remove profile simulation button

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -2,7 +2,7 @@ import { FunctionalComponent } from 'preact'
 import { RoundedBoxContainer } from '../../components/containers/RoundedBoxContainer'
 import { Heading1 } from '../../components/typography/Heading1'
 import { useEffect, useState } from 'preact/hooks';
-import { fetchWrapper } from '../../wrappers/fetchWrapper';
+import { apiUrl, fetchWrapper } from '../../wrappers/fetchWrapper';
 
 export const Profile: FunctionalComponent = () => {
     const [apiToken, setApiToken] = useState('');
@@ -31,11 +31,11 @@ export const Profile: FunctionalComponent = () => {
                     readOnly
                 />
                 <p class="mt-2 text-gray-600 text-sm">
-                    Use this token to post events to <code>apiUrl/events</code> with DTO <code>{'{'}name: eventName{'}'}</code> and header <code>Authorization "API {apiToken}"</code>.
+                    Use this token to post events to <code>{`${apiUrl}`}/events</code> with DTO <code>{'{'}"name": "my-event-name"{'}'}</code> and header <code>Authorization "API {apiToken}"</code>.
                 </p>
             </div>
 
-            <hr class="my-6" />
+            {/* <hr class="my-6" />
 
             <button
                 class="w-full mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -45,7 +45,7 @@ export const Profile: FunctionalComponent = () => {
 
             <p class="text-gray-600">
                 Simulation data will be used to view in the events overview. This is just for experimentation purposes.
-            </p>
+            </p> */}
         </RoundedBoxContainer>
     )
 }
