4d496e4672e00d061fa7b55e7e8aa46b2e4c5e86
Marius Fagerland <mariusfagerland@yahoo.no>
2023-08-11 17:07:31 +0200

    Fix typos in profile

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -31,21 +31,9 @@ export const Profile: FunctionalComponent = () => {
                     readOnly
                 />
                 <p class="mt-2 text-gray-600 text-sm">
-                    Use this token to post events to <code>{`${apiUrl}`}/events</code> with DTO <code>{'{'}"name": "my-event-name"{'}'}</code> and header <code>Authorization "API {apiToken}"</code>.
+                    Use this token to post events to <code>{`${apiUrl}`}/event</code> with DTO <code>{'{'}"name": "my-event-name"{'}'}</code> and header <code>Authorization "Api {apiToken}"</code>.
                 </p>
             </div>
-
-            {/* <hr class="my-6" />
-
-            <button
-                class="w-full mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-                Simulate Data
-            </button>
-
-            <p class="text-gray-600">
-                Simulation data will be used to view in the events overview. This is just for experimentation purposes.
-            </p> */}
         </RoundedBoxContainer>
     )
 }
