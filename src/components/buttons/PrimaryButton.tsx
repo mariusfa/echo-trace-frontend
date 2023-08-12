96067afb75e1dd95c99d309bd0b5961810a0dc50
Marius Fagerland <mariusfagerland@yahoo.no>
2023-08-12 14:03:00 +0200

    Update primary button color to be UU friendly

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const PrimaryButton: FunctionalComponent<Props> = ({ label, onClick, type = "button" }) => {
     return (
         <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
+            class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
             type={type}
             onClick={onClick}
         >
